2085b5a9ea1e562c7ab400bd8215e731c1db12eb
Chinwendu joy Enyidiegwu <chinwe640@gmail.com>
2023-05-20 12:08:02 +0100

    styles

--- app/page.jsx
+++ app/page.jsx
@@ -4,14 +4,14 @@ const Home = () => {
   return (
 		<section className="w-full flex-center flex-col">
 			<h1 className="head_text text-center">
-				Discover & Share
+				Generate & Share
 				<br className="max-md:hidden" />
-				<span className="orange_gradient text-center">
+				<span className="green_gradient text-center">
 					AI - Powered Prompts
 				</span>
 			</h1>
 			<p className="desc text-center">
-				promtopia is an open-source AI prompting tool for modern world to
+				promptAI is an open-source AI prompting tool for modern world to
 				discover, create and share creative prompts
 			</p>
 
